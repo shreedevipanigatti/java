b9e272843f11c5d48af7efeb09959631515aef48
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-16 10:51:10 +0530

    first commit

---
+++ Namaskara.java
@@ -0,0 +1,25 @@
+class Wish
+{
+//main method is an entery point
+public static void main(String[] shree )
+{
+System.out.println("Namaskra");
+}
+}
+
+class Camera
+{
+public static void main(String[] camera)
+{
+System.out.println("Camera is kodak");
+System.out.println("Price is lot");
+}
+}
+
+class Weather
+{
+public static void main(String[] jacket)
+{
+System.out.println("City");
+}
+}
