61c4436856a5c81e0307c0e216b7447586cba50e
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-17 18:16:26 +0530

    third commit

---
+++ Works3.java
@@ -0,0 +1,20 @@
+class Name
+{
+	public static void main(String[] Shree)
+	{ 
+	System.out.println("X-workz");
+	write();
+	write();
+	write();
+	}
+	public static void write() 
+	{
+		System.out.println("Laptop");
+		
+		
+	}
+}
+	
+	
+	
+	
