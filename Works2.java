2c02af7ec24c4996026b52b5777e60f100c68a55
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-17 18:15:01 +0530

    Add files via upload

---
+++ Works2.java
@@ -0,0 +1,32 @@
+class Hello
+{
+public static void main(String[] Ask)
+{
+System.out.println("Something");
+ washing();
+ } 
+ 
+public static void washing() 
+{
+System.out.println("Clothes");
+gardning();
+}
+public static void gardning()
+{
+	System.out.println("Tress");
+
+ read();
+ } 
+ 
+public static void read() 
+{
+System.out.println("Books");
+packing();
+}
+public static void packing()
+{
+	System.out.println("Bag");
+} 
+}
+
+
