147efcfcddb73e881a2e00a630309a32ca832239
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-19 23:09:45 +0530

    Add files via upload

---
+++ Home.java
@@ -0,0 +1,61 @@
+public class Home
+{
+public static void main(String[] Names)
+{
+System.out.println("familyMembers");
+byte numberOfMembers;
+numberOfMembers= 6;
+System.out.println(numberOfMembers);
+
+System.out.println("minutesInADay");
+short minutesInADay;
+minutesInADay= 1440;
+System.out.println(minutesInADay);
+
+System.out.println("students");
+int numbersOfEmployersInBengalur;
+numbersOfEmployersInBengalur= 1000000;
+System.out.println(numbersOfEmployersInBengalur);
+
+System.out.println("area");
+long areaOfEarth;
+areaOfEarth= 510100000;
+System.out.println(areaOfEarth);
+
+System.out.println("percentage");
+float highesttwodegitdecimalNr;
+highesttwodegitdecimalNr = 90.99f;
+System.out.println(highesttwodegitdecimalNr);
+
+System.out.println("chargeOf");
+double chargeOfElectron;
+chargeOfElectron= 1.620217662000000000000;
+System.out.println(chargeOfElectron);
+ 
+System.out.println("sweet");
+boolean sugarIsBitter;
+sugarIsBitter = false;
+System.out.println(sugarIsBitter);
+ 
+System.out.println("name");
+char sugar;
+sugar = 'S';
+System.out.println(sugar);
+  
+System.out.println("fruit");
+String fruit;
+fruit= "Banana";
+System.out.println(fruit);
+}
+}
+  
+ 
+ 
+
+
+
+
+
+
+
+
