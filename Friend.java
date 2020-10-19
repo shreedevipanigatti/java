556898ef873921da3a19d2b36ab619c7c08ef8a3
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-19 23:11:09 +0530

    Add files via upload

---
+++ Friend.java
@@ -0,0 +1,61 @@
+public class Friend
+{
+public static void main(String[] Name)
+{
+System.out.println("Bright");
+byte priceOfPen;
+priceOfPen= 5;
+System.out.println(priceOfPen);
+
+System.out.println("deaths");
+short numbersOfCarsInBelagavi;
+numbersOfCarsInBelagavi= 30000;
+System.out.println(numbersOfCarsInBelagavi);
+
+System.out.println("pepoles");
+int numbersOfUnEmployersInIndia;
+numbersOfUnEmployersInIndia= 200000;
+System.out.println(numbersOfUnEmployersInIndia);
+
+System.out.println("cases");
+long numbersOfCaronaCasesInWorld;
+numbersOfCaronaCasesInWorld= 1066000000;
+System.out.println(numbersOfCaronaCasesInWorld);
+
+System.out.println("online");
+float ratingOfXworkz;
+ratingOfXworkz = 4.9f;
+System.out.println(ratingOfXworkz);
+
+System.out.println("value");
+double valueOfPi;
+valueOfPi= 3.14;
+System.out.println(valueOfPi);
+ 
+System.out.println("month");
+boolean sevenDaysPerWeek;
+sevenDaysPerWeek =false;
+System.out.println(sevenDaysPerWeek);
+ 
+System.out.println("name");
+char god;
+god = 'G';
+System.out.println(god);
+  
+System.out.println("flower");
+String flower;
+flower= "Rose";
+System.out.println(flower);
+}
+}
+  
+ 
+ 
+
+
+
+
+
+
+
+
