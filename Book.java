5cbb6b4428d6910dca4f996200b6a1dc968e1ac0
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-19 23:14:16 +0530

    third commit

---
+++ Book.java
@@ -0,0 +1,61 @@
+public class Book
+{
+public static void main(String[] Name)
+{
+System.out.println("Bright");
+byte priceOfPen;
+priceOfPen= 5;
+System.out.println(priceOfPen);
+
+System.out.println("salaryPerMonth");
+short salaryPerMonth;
+salaryPerMonth= 30000;
+System.out.println(salaryPerMonth);
+
+System.out.println("pepoles");
+int numbersOfTeachersInIndia;
+numbersOfTeachersInIndia= 200000;
+System.out.println(numbersOfTeachersInIndia);
+
+System.out.println("Things");
+long numbersOfLivingThings;
+numbersOfLivingThings= 1566000000;
+System.out.println(numbersOfLivingThings);
+
+System.out.println("rating");
+float ratingOfMovie;
+ratingOfMovie = 8.14f;
+System.out.println(ratingOfMovie);
+
+System.out.println("speedOfLight");
+double speedOfLight;
+speedOfLight= 300000000;
+System.out.println(speedOfLight);
+ 
+System.out.println("days");
+boolean sevenDaysInOneWeek;
+sevenDaysInOneWeek = true;
+System.out.println(sevenDaysInOneWeek);
+ 
+System.out.println("letter");
+char copy;
+copy = 'C';
+System.out.println(copy);
+  
+System.out.println("animalName");
+String animalName;
+animalName= "Dog";
+System.out.println(animalName);
+}
+}
+  
+ 
+ 
+
+
+
+
+
+
+
+
