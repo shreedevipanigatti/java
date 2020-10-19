fe09293fe2b38275d5f47e05f288ca48664fd382
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-19 23:07:52 +0530

    first commit

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student{
+
+public static void main(String[] Parts)
+{
+System.out.println("fingers");
+byte numberOfFingers;
+numberOfFingers=20;
+System.out.println(numberOfFingers);
+
+System.out.println("FamilyMembers");
+byte numberOfMembers;
+numberOfMembers=6;
+System.out.println(numberOfMembers);
+
+System.out.println("Bright");
+byte price;
+priceOfPen=5;
+System.out.println(priceOfPen);
+}
+}
+
+
+
