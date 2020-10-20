e8c3e4dbbaffc3ff94cded0b9593179c8b35c09e
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-20 20:36:27 +0530

    Add files via upload

---
+++ Plant.java
@@ -0,0 +1,19 @@
+public class Plant
+ {
+public static void main(String[] Lotus)
+{
+	System.out.println("Shree");
+	printEmail("shreedevipanigatti.xworkz@gmail.com");
+	printProgrammingLanguage("java");
+}
+public static void printEmail(String email)
+{
+  System.out.println("Email :"+email);
+}
+public static void printProgrammingLanguage(String programmingLanguage) 
+{
+System.out.println(" programmingLaunguage :"+programmingLanguage);
+  }
+}
+  
+  
