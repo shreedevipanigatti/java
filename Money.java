438b6357936e322d52856c6ce79f97b76750a764
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:29:47 +0530

    fourth commit

---
+++ Money.java
@@ -0,0 +1,18 @@
+public class Money
+{
+public static void main(String[] shree)
+{
+String phoneNumber="7026855968";
+
+System.out.println("phoneNumber : "+phoneNumber);
+
+String email="shreebp20@gmail.com";
+
+System.out.println("email : "+email);
+
+ final String adharNumber="13896889905";
+
+System.out.println("adharNumber : "+adharNumber);
+
+}
+}
