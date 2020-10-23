a949d954ac17d9e62f8957fa92ef8324c33034d2
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:03:24 +0530

    first commit

---
+++ Pubg.java
@@ -0,0 +1,19 @@
+public class Pubg {
+ 
+ public static void supply()
+ {
+ 
+ System.out.println("invoked supply from Pub-g class");
+ }
+ 
+ public static void enemiesAhead()
+ {
+ 
+ System.out.println("invoked enemiesAhead from Pub-g class");
+ }
+ }
+ 
+ 
+ 
+ 
+
