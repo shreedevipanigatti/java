b5a23d4cc88c5e1709642f4d83a77921aad4527b
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:26:06 +0530

    fourth commit

---
+++ Body.java
@@ -0,0 +1,30 @@
+public class Body{
+
+public static void main(String[] Parts)
+{
+System.out.println("fingers");
+byte numberOfFingers;
+numberOfFingers=20;
+System.out.println(numberOfFingers);
+}
+public class Family{
+
+public static void main()
+{
+System.out.println("Membersname");
+byte numberOfMembers;
+numberOfMembers=6;
+System.out.println(numberOfMembers);
+}
+public class pen {
+public static void main( )
+{
+System.out.println("Bright");
+byte price;
+price=5;
+System.out.println(price);
+}
+}
+}
+}
+
