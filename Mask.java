3b54e36a50feb1edd6e2694080076c9598f3f8a7
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:05:21 +0530

    third commit

---
+++ Mask.java
@@ -0,0 +1,17 @@
+public class Mask {
+ 
+ public static void health()
+ {
+ 
+ System.out.println("invoked supply from Mask class");
+ }
+ 
+ public static void umbrella()
+ {
+ 
+ System.out.println("invoked umbrella from Mask class");
+ }
+ }
+ 
+ 
+ 
