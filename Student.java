a8b69a45d819151163a98842f0f6ca443e42a098
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:24:59 +0530

    third commit

---
+++ Student.java
@@ -0,0 +1,64 @@
+public class Student{
+
+public static void main(String[] Parts)
+{
+System.out.println("fingers");
+byte numberOfFingers;
+numberOfFingers=20;
+System.out.println(numberOfFingers);
+
+System.out.println("days");
+short daysInYear;
+daysInYear= 365;
+System.out.println(daysInYear);
+
+System.out.println("students");
+int numbersOfEngineeringsInIndia;
+numbersOfEngineeringsInIndia= 1000000;
+System.out.println(numbersOfEngineeringsInIndia);
+
+System.out.println("population");
+long populationOfIndia;
+populationOfIndia= 1366000000;
+System.out.println(populationOfIndia);
+
+System.out.println("percentage");
+float percentageInSslc;
+percentageInSslc = 90.14f;
+System.out.println(percentageInSslc);
+
+System.out.println("chargeOfElectron");
+double chargeOfElectron;
+chargeOfElectron= 1.620217662000000000000;
+System.out.println(chargeOfElectron);
+ 
+System.out.println("Earth");
+boolean earthIsFlat;
+earthIsFlat = false;
+System.out.println(earthIsFlat);
+ 
+System.out.println("marks");
+char grade;
+grade = 'A';
+System.out.println(grade);
+  
+System.out.println("name");
+String name;
+name= "Shree";
+System.out.println(name);
+}
+}
+  
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
