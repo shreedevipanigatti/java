27a24ed16b8b203a85cd3fd6ba0d9a68d932f708
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:06:09 +0530

    fourth commit

---
+++ Photo.java
@@ -0,0 +1,15 @@
+public class Photo {
+ 
+ public static void main(String[] camera)
+ {
+ 
+ System.out.println("JVM invoked main from Photo");
+ 
+ Mask.health();
+ 
+ Mask.umbrella();
+ 
+ System.out.println("EXIT :: main from Photo");
+ }
+ }
+ 
