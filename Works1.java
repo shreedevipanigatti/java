bb0ce2c8bbca06f11d2979bc0a666b21edd3a1f9
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-23 12:27:16 +0530

    first commit

---
+++ Works1.java
@@ -0,0 +1,19 @@
+class WorksName
+{
+public static void main(String[] ask)
+{
+System.out.println("Some Help");
+ music();
+ } 
+ 
+public static void music() 
+{
+System.out.println("Guitar");
+drinking();
+}
+public static void drinking()
+{
+	System.out.println("water");
+} 
+
+}
