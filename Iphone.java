41b4c98a8d77c295fc4cb9c17ee9104cdc9d4f83
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-28 00:07:00 +0530

    first commit

---
+++ Iphone.java
@@ -0,0 +1,34 @@
+public class Iphone {
+ 
+ public static boolean call()
+ {
+  
+  System.out.println("invoked call");
+  boolean connected=true;
+  return connected;
+ }
+ 
+  public static String message()
+  {
+  System.out.println("invoked message");
+  String msg="Good Moring"; 
+  return msg;
+ 
+  } 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
