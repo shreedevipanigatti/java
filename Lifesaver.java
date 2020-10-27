0b722ca06fbc0876ed8237d5bc379cf9dd9230f3
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-10-28 00:10:19 +0530

    second commit

---
+++ Lifesaver.java
@@ -0,0 +1,13 @@
+public class HumanLife
+{
+	public static void main(String[] test)
+	{
+		System.out.println("Invoked by JVM");
+		
+		String CheckAge=HumanLife.ageFormat();
+		
+		System.out.println("the age format is:" +CheckAge);
+	
+	System.out.println("exit: main");
+	}
+}
