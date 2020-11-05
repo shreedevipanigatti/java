788595518cc25a7c5286bd07cb4eca42c876b003
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-05 23:38:47 +0530

    first test file

---
+++ AddressTester.java
@@ -0,0 +1,20 @@
+public class AddressTester
+{
+public static void main (String[] valves)
+{
+System.out.println("JVM invoked main");
+short door=Address.doorNo;
+System.out.println("door"+door);
+
+String st=Address.street;
+System.out.println("st"+st);
+
+System.out.println("pincode"+Address.pincode);
+
+System.out.println("city"+Address.city);
+
+
+System.out.println("EXIT :: main");
+
+}
+}
