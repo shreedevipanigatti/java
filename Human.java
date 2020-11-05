6bcf7f69a575877a465ec0e590bbb7c3d662f1ec
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-05 23:43:56 +0530

    default valves of all primitive

---
+++ Human.java
@@ -0,0 +1,14 @@
+public class Human {
+	
+public static byte age;
+public static short daysInAYear;
+public static int salary;
+public static long adharNo;
+public static float hight;
+public static double percentage;
+public static boolean alwaysEatSilverSpoon;
+public static char bloodGroup;
+public static String name;
+
+
+}
