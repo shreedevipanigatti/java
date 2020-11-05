6599841ba2eef049f164dcbcf8c77d25ece5338b
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-05 23:40:15 +0530

    second commit

---
+++ Country.java
@@ -0,0 +1,10 @@
+public class Country {
+	
+public static String name;
+public static long population;
+public static byte flagColours;
+public static String indianAnimal;
+public static byte noOfStates;
+
+
+}
