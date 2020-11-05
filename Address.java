48354ac03971811889cf65852ce1a3a6bb6d26bd
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-05 23:37:32 +0530

    first commit

---
+++ Address.java
@@ -0,0 +1,9 @@
+public class Address {
+	
+public static short doorNo=425;
+public static String street="Kamatageri street";
+public static int pincode=582201;
+public static String city="Dharwad";
+
+
+}
