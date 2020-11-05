a5699423488e716c0d210aef9f771e265c022a45
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-05 23:45:08 +0530

    all primitives test file

---
+++ HumanTester.java
@@ -0,0 +1,32 @@
+public class HumanTester
+{
+public static void main (String[] living)
+{
+System.out.println("JVM invoked main");
+byte ages=Human.age;
+System.out.println("ages"+ages);
+
+short days=Human.daysInAYear;
+System.out.println("days"+days);
+
+System.out.println("salary"+Human.salary);
+
+System.out.println("adharNo"+Human.adharNo);
+
+System.out.println("hight"+Human.hight);
+
+System.out.println("percentage"+Human.percentage);
+
+System.out.println("alwaysEatSilverSpoon"+Human.alwaysEatSilverSpoon);
+
+System.out.println("bloodGroup"+Human.bloodGroup);
+
+//String name=Human.name;
+//System.out.println("name"+name);
+
+System.out.println("name"+Human.name);
+
+System.out.println("EXIT :: main");
+
+}
+}
