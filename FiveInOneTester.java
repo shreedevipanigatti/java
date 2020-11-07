5295c1f177ab129c024edbe1160ad6a6352a2152
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-07 07:21:32 +0530

    5Ex tester

---
+++ FiveInOneTester.java
@@ -0,0 +1,47 @@
+public class FiveInOneTester
+{
+public static void main (String[] fields)
+{
+System.out.println("JVM invoked main");
+
+byte building=Building.length;
+System.out.println("length"+building);
+short builds=Building.height;
+System.out.println("height"+builds);
+System.out.println("price"+Building.price);
+System.out.println("colour"+Building.colour);
+System.out.println("material"+Building.material);
+
+System.out.println("capacity"+SchoolBag.capacity);
+System.out.println("price"+SchoolBag.price);
+System.out.println("size"+SchoolBag.size);
+System.out.println("colour"+SchoolBag.colour);
+System.out.println("quality"+SchoolBag.quality);
+
+
+System.out.println("size"+Table.size);
+System.out.println("price"+Table.price);
+System.out.println("capacity"+Table.capacity);
+System.out.println("material"+Table.material);
+System.out.println("paper"+Table.paper);
+
+
+System.out.println("stateNmae"+States.stateName);
+System.out.println("population"+States.population);
+System.out.println("noOfDistrict"+States. noDistrict);
+System.out.println("noOfStatesInCountry"+States.noOfStesInCountry);
+System.out.println("noOfTaluksInDharwad"+States.noOfTaluksInDharwad);
+
+
+System.out.println("noOfPlayers"+Cricket.noOfPlayers);
+System.out.println("teamName"+Cricket.teamName);
+System.out.println("rcbCaptain"+Cricket.rcbCaptain);
+System.out.println("colourofRcbShirt"+Cricket.colourOfRcbShirt);
+System.out.println ("winAmount"+Cricket. winAmount);
+
+
+
+System.out.println("EXIT :: main");
+
+}
+}
