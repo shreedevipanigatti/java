bef2cb7edf418a6eddfde7b869d3181c9e699cc5
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-07 07:23:41 +0530

    5Ex fourth file

---
+++ States.java
@@ -0,0 +1,10 @@
+public class States {
+	
+public static String stateName="Karnataka";
+public static long population=2000000;
+public static byte noDistrict=30;;
+public static byte noOfStesInCountry=29;
+public static byte noOfTaluksInDharwad=7;
+
+
+}
