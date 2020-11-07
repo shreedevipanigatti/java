b519e3766df7e1266d23f7b43ee840e59a2539ba
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-07 07:24:20 +0530

    5Ex fifth file

---
+++ Cricket.java
@@ -0,0 +1,11 @@
+public class Cricket {
+	
+public static byte  noOfPlayers=24;
+public static  String teamName="Rcb";
+public static String rcbCaptain="ViratKohli";
+public static String colourOfRcbShirt="Black";
+public static int winAmount=1000000;
+
+
+}
+
