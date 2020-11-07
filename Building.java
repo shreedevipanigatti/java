d54f48045bbbf4c95460627e8f262b38efcb3367
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-07 07:20:31 +0530

    5Ex first file

---
+++ Building.java
@@ -0,0 +1,12 @@
+public class Building {
+	
+public static byte length=120;
+public static short height=225;
+public static long price=1500000;
+public static String colour="White";
+public static String material="HighQuality";
+
+
+}
+
+
