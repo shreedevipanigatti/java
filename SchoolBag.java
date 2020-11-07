e0ef5c2d9dd7d694f01c818b58a8049c77f86953
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-07 07:22:29 +0530

    5Ex second file

---
+++ SchoolBag.java
@@ -0,0 +1,11 @@
+public class SchoolBag {
+	
+public static byte capacity=125;
+public static short price=5000;
+public static byte size=75;
+public static String colour="Black";
+public static String quality="Good";
+
+
+}
+
