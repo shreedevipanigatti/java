e798f7f7f7f8152ee5306a27253edba66531e888
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-07 07:23:07 +0530

    5Ex third file

---
+++ Table.java
@@ -0,0 +1,10 @@
+public class Table {
+	
+public static byte size=75;
+public static short price=5000;
+public static int capacity=250000;
+public static String material="Good";
+public static String paper="NewsPaper";
+
+
+}
