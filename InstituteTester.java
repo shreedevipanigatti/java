47f1d47ac670b4081823ebbb835cc6b8a1b0a7b7
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-08 20:52:10 +0530

    third programme test file

---
+++ InstituteTester.java
@@ -0,0 +1,17 @@
+public class InstituteTester{
+
+
+public static void main(String[] photos)
+{
+
+System.out.println("invoked main, doing switch and enum");
+
+InstituteFinder.lowestAndBestUsingString("whatver ");
+	//String instituteName="ELITMUS";
+	//Institute enumInstituteName=Institute.valueOf(instituteName);
+InstituteFinder.lowestAndBestUsingEnum(Institute.XWORKZ);
+
+System.out.println("EXIT :: main, doing switch and enum");
+}
+
+}
