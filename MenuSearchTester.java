bf4f69e789f62db5c7e59cd507b7f2679bb385c7
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-17 19:21:58 +0530

    third test programme

---
+++ MenuSearchTester.java
@@ -0,0 +1,16 @@
+package com.xworkz.online;
+
+public class MenuSearchTester {
+
+	public static void main(String[] args) {
+
+		System.out.println("jvm kareyuvudu main");
+
+		System.out.println("Total items to search" + HotelMenu.totalItems());
+		HotelMenu.search("Dosa");
+
+		System.out.println("main nirgamana");
+
+}
+}
+
