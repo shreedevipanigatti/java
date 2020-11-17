97cdf7f24d9d4a265bc00c36d13d839526a15874
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-17 19:23:38 +0530

    fourth test programme-non-13

---
+++ PhoneNumbers.java
@@ -0,0 +1,12 @@
+package com.xworkz.online;
+
+public class PhoneNumbers {
+
+	public static void main(String[] args) {
+
+		System.out.println("Total numbers to search" + PhoneDirectory.totalNumbers());
+		PhoneDirectory.search(7123450298L);
+
+	}
+
+}
