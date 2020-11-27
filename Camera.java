ff360e430ed59e2717a4be75116339df1205b83d
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-27 08:17:56 +0530

    memory task 1

---
+++ Camera.java
@@ -0,0 +1,14 @@
+package com.xworkz.memory.copies;
+
+public class Camera {
+
+	public static String brand="Panasonic";
+	public static String lenses ="Interchangable";
+	public String type="DSLR";
+	public String colour="Brown";
+	public int price= 50000;
+	public String carryable="yes";
+}
+
+
+
