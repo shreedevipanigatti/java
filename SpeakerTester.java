0a54cad31e44f778e894266b99ced35bef2f62a0
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-27 08:17:10 +0530

    memory task 2 tester

---
+++ SpeakerTester.java
@@ -0,0 +1,17 @@
+package com.xworkz.memory.copies;
+
+import com.xworkz.memory.copies.Speaker;
+public class SpeakerTester {
+	
+	public static void main(String[] args) {
+	
+	        Speaker speaker=new Speaker();
+	        System.out.println(speaker.brand);
+	 		System.out.println(speaker.price);
+	 		System.out.println(speaker.type);
+	 	}
+
+	 }
+
+
+
