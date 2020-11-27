62c74325da8b7f4a97a118123adbd5c58f7a7d81
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-27 08:18:35 +0530

    memory task 1 tester

---
+++ CameraTester.java
@@ -0,0 +1,26 @@
+package com.xworkz.memory.copies;
+
+import com.xworkz.memory.copies.Camera;
+public class CameraTester {
+
+	
+      public  void main(String[] args) {
+			Camera camera1=new Camera();
+			System.out.println("camera1.colour");
+		//	Camera.colour="Black";
+			System.out.println("Camera.colour");
+			System.out.println("Camera.type");
+			System.out.println("Camera.price");
+			
+			System.out.println("Camera.brand");
+			System.out.println("Camera.carryable");
+			
+			//Camera camera1 =new Camera();
+			camera1.colour="red";
+			System.out.println(camera1.colour);
+		//	System.out.println(camera1.lenses);
+
+		}
+	}
+
+
