a8b49414c1c3ecf4641b6153427b27d3eea7241b
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-29 22:20:58 +0530

    3rd task test-27

---
+++ MuseumTester.java
@@ -0,0 +1,11 @@
+
+package com.xworkz.copies.constructor;
+import com.xworkz.copies.constructor.Museum;
+public class MuseumTester {
+
+		public static void main(String[] args) {
+		Museum museum=new Museum("Art museum","Banglore");
+
+		}
+
+	}
