effd0d1069d79a78b29a4920a85d48e904beac5b
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-29 22:19:16 +0530

    2 nd task test-27

---
+++ CoffeTester.java
@@ -0,0 +1,11 @@
+package com.xworkz.copies.constructor;
+
+import com.xworkz.copies.constructor.Coffe;
+
+public class CoffeTester {
+	
+	public static void main(String[] args)
+    Coffe bru=new Coffe("BRU",100);
+	System.out.println(bru.company);
+	System.out.println(bru.grams);
+}
