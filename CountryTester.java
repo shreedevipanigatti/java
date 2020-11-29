608f2302208402d8097694dafc087ba6beeacd72
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-29 22:22:00 +0530

    4th task test-27

---
+++ CountryTester.java
@@ -0,0 +1,23 @@
+package com.xworkz.copies.constructor;
+import com.xworkz.copies.constructor.country;
+public class CountryTester {
+
+	public static void main(String[] args) {
+			
+					String colour="4";
+					byte num=Byte.parseByte(colour);
+					String states="28";
+					byte num1=Byte.parseByte(states);
+					String nationalFlower="lotus";
+					short num2=Short.parseShort(nationalFlower);
+					
+					
+					Country country=new Country("India","tiger",4,28,"Naremdra Modi","Hockey","lotus","Peacock");
+		            
+				}
+
+			}
+			
+	}
+
+}
