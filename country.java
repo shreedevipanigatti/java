36322fe4d558d0a843600d49e2066c20aa861058
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-29 22:21:29 +0530

    4th task -27

---
+++ country.java
@@ -0,0 +1,27 @@
+package com.xworkz.copies.constructor;
+
+public class country {
+	
+            public String nameofcountry;
+			public String nationalAnimal;
+			public byte noOfColoursInFlag;
+			public byte noOfStates;
+			public String primeMinisterName;
+			public String nationalGame;
+			public String nationalFlower;
+			public String nationalBird;
+		public Country(String nameOfcountry,String animal,byte colour,byte states,String nameOfPrimeMinister,String game,String nationalFlower,String bird)
+		{
+			System.out.println("name of the country::" +nameOfcountry);
+			System.out.println("national animal::" +animal);
+			System.out.println("number of colours in flag::" +colour);
+			System.out.println("number states ::" +states);
+			System.out.println("nationalgame::" +game);
+			System.out.println("nationalFlower"::" +flower);
+			System.out.println("Birdname::" +bird);
+			
+		}
+
+	}
+
+
