225711189b467661b04fff2c74e10d9abeeb4977
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-11-29 22:20:28 +0530

    3rd task-27

---
+++ Museum.java
@@ -0,0 +1,27 @@
+
+		package com.xworkz.cpoies.constructor;
+
+		public class Museum {
+				public String museumName="Karnataka Chitrakala Parishat";
+				public String city="Brngalore";
+			
+
+			public Museum(String name,String place)
+			{
+				System.out.println("Invoked Museum Constructor");
+				System.out.println("name of museum ::" +museumType);
+				System.out.println("name of place::" +city);
+			    museumType=name;
+				city=place;
+				
+				System.out.println("name of museum After::" +name);
+				System.out.println("name of place After::" +place);
+		
+			}
+				
+
+			}
+
+	
+
+
