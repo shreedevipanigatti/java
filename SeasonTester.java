8c4cd7d3e06bef4c835318fbe075226b13c3aa0c
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-12-02 23:04:56 +0530

    task 2 test file dec1

---
+++ SeasonTester.java
@@ -0,0 +1,20 @@
+package com.xworkz.copies.constructor;
+import com.xworkz.copies.constructor.Season;
+
+public class SeasonTester {
+	
+	
+	//@SuppressWarnings("unused")
+	
+		public static void main(String args[])
+		{
+			Season seasons=new Season("SUMMER","APRIAL");
+			
+			System.out.println(seasons.name);	
+		    System.out.println(seasons.startMonth);
+		}
+		    
+	}
+
+
+
