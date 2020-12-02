832cfaa8915f247bd0dd25bf94943b763a93caf9
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-12-02 23:04:23 +0530

    task2 dec1

---
+++ Season.java
@@ -0,0 +1,18 @@
+package com.xworkz.copies.constructor;
+
+public class Season {
+ 
+	
+		public String name;
+		public String startMonth;
+
+		public Season(String name, String startMonth)
+		{
+		
+		this.name=name;
+		this.startMonth=startMonth;
+	}
+	}
+
+
+
