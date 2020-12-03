4ea67b2569e9fc88322c3407d2744e0d8b380407
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-12-03 22:58:20 +0530

    speaker test file dec3

---
+++ SpeakerTester.java
@@ -0,0 +1,40 @@
+         package com.xworkz.method;
+
+		import com.xworkz.method.electronic.Speaker;
+
+		public class SpeakerTester {
+
+			public static void main(String[] args) {
+				
+				Speaker speaker=new Speaker("Gray",8);
+				System.out.println(speaker.getCompanyName());
+				System.out.println(speaker.isConnected());
+				
+				speaker.onOrOff();
+				
+				speaker.increaseVolume();
+				speaker.increaseVolume();
+				speaker.increaseVolume();
+				speaker.increaseVolume();
+				speaker.increaseVolume();
+				speaker.decreaseVolume();
+				
+				speaker.onOrOff();
+				
+				speaker.increaseVolume();
+				speaker.decreaseVolume();
+				
+				speaker.onOrOff();
+				
+				speaker.decreaseVolume();
+				speaker.decreaseVolume();
+				speaker.decreaseVolume();
+				speaker.decreaseVolume();
+				speaker.decreaseVolume();
+				speaker.increaseVolume();
+				
+			}
+
+		}
+
+
