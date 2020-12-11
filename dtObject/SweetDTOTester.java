5931485efe2f25c6de45ab6fa4308293019f54d2
shreedevipanigatti <72909814+shreedevipanigatti@users.noreply.github.com>
2020-12-11 19:42:23 +0530

    Update SweetDTOTester.java

--- dtObject/SweetDTOTester.java
+++ dtObject/SweetDTOTester.java
@@ -1,26 +1,32 @@
 package com.xworkz.dtObject;
 
-import com.xworkz.dtObject.DTO.SweetDTO;
+
+import java.util.Arrays;
+
+import com.xworkz.DTO.sweet.Shape;
+import com.xworkz.DTO.sweet.SweetDTO;
 
 public class SweetDTOTester {
 
 	public static void main(String[] args) {
-		SweetDTO sweetDto = new SweetDTO("Dharawad Peda", 250);
-		System.out.println(sweetDto.getName());
-		System.out.println(sweetDto.getPrice());
-		//sweetDto.setPrice(550);
-		//System.out.println("updated price  " + sweetDto.getPrice());
+
+		SweetDTO sweetDTO = new SweetDTO();
+		System.out.println(sweetDTO.getName());
+		System.out.println(sweetDTO.getPopularState());
+		System.out.println(sweetDTO.getPrice());
+		System.out.println(" ");
 
 		String[] color = new String[2];
-		color[0] = "brown";
-		color[1] = "chakolet";
+		color[0] = "Chocolate";
+		color[1] = "Brown";
 
-		SweetDTO sweetDto2 = new SweetDTO("Karnataka", "1KG", color);
-		System.out.println(sweetDto2.getPopularState());
-		System.out.println(sweetDto2.getQuantity());
-		System.out.println(sweetDto2.getColor());
-
-		System.out.println(sweetDto2.getShape());
-
+		SweetDTO sweetDTO2 = new SweetDTO("DharwadPeda", "Karnataka", color, Shape.ROUND);
+		System.out.println(sweetDTO2.getName());
+		System.out.println(sweetDTO2.getPopularState());
+		System.out.println(Arrays.toString(color));
+		System.out.println(sweetDTO2.getShape());
+		sweetDTO2.setPrice(250`);
+		System.out.println(sweetDTO2.getPrice());
 	}
+
 }
